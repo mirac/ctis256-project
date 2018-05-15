be64379702068586e7bfff5e6afe0239a0880db1
Miraç Satıç <miracsa@yandex.com>
2018-05-16 00:40:38 +0300

    db config fixed

--- db.php
+++ db.php
@@ -7,11 +7,8 @@ $pass = '321';
 try {
   $db = new PDO ($dsn, $user, $pass);
   $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch ( Exception $ex){
-<<<<<<< HEAD
-
-=======
->>>>>>> a87d28a703953bfc963af28eaf0817a4772b1164
-  echo "<p>DB Connection Error</p>";
-  exit;
+} catch ( Exception $ex) {
+    echo "<p>DB Connection Error</p>";
+    exit;
+}
 }
