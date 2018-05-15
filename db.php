efb64e59c51413b2259ff1ae11f1fe4c40ef3cee
Miraç Satıç <miracsa@yandex.com>
2018-05-16 00:34:54 +0300

    DB class fixed

--- db.php
+++ db.php
@@ -2,13 +2,12 @@
 
 $dsn = 'mysql:host=localhost;dbname=test;charset=utf8mb4';
 $user = 'root';
-$pass = '';
+$pass = '321';
 
 try {
   $db = new PDO ($dsn, $user, $pass);
   $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch ( Expression $ex){
-
+} catch ( Exception $ex){
   echo "<p>DB Connection Error</p>";
   exit;
 }
