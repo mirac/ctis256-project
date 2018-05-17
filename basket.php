849be57fe68f8d97279472083f9fcf984f152842
Cagri <caggri@outlook.com>
2018-05-17 19:34:12 +0300

    db moved to public

--- basket.php
+++ basket.php
@@ -3,6 +3,12 @@
 <?php
 
 require_once 'include/header.php';
+require_once 'include/db.php';
+$stmt = $db->prepare("select * from products where promotional = 1 ");
+$stmt -> execute();
+$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
 
 <body>
@@ -10,28 +16,64 @@ require_once 'include/header.php';
     <div id="basketmain">
 
         <div id="basketleft">
-
             <h1>Basket</h1>
 
+            <div id="basketleftinner">
 
-                <?php
 
-                foreach ($stmt as $item) {
-                    echo '<div id="basketitems">';
-                    echo '<img src="' . $item['image'] . '">';
-                    echo '</div>';
-                }
-                ?>
+                    <?php
+                    foreach ($list as $item) {
 
+                       echo ' <table><tr>';
+                        echo '<td><img id="basketimg" src="' . $item['image'] . '"></td>';
+
+                        echo '<td ><span id="baskettitle">'. $item['title'] . '</span> </td>';
+                        //echo '<td><img src="' . $amount . '"></td>>'; This is amount part
+                        //echo '<td><img src="' . $item['price'] * amount . '"></td>>';  price * amount
+                        echo '<td ><span id="baskettitle">amount</span></td>';
+                        echo '<td ><span id="baskettitle">' . $item['price']  . '</span></td>';
+
+                        echo '</tr></table><br>';
+
+
+                    }
+                    ?>
+
+
+            </div>
 
         </div>
         <div id="basketright">
 
-            <p>Deneme</p>
+            <h1>Checkout</h1>
+            <div id="payout">
 
-        </div>
+                Total price:
 
-    </div>
+
+                Payment Method:
+
+
+
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-primary">
+                        <input type="radio" name="options" id="option1" autocomplete="off"><i class="fab fa-cc-paypal fa-4x"></i>
+
+
+
+                    </label>
+                    <label class="btn btn-primary">
+                        <input type="radio" name="options" id="option2" autocomplete="off"> <i class="fab fa-cc-mastercard fa-4x"></i>
+                    </label>
+                    <label class="btn btn-primary">
+                        <input type="radio" name="options" id="option3" autocomplete="off">  <i class="fab fa-cc-visa fa-4x" aria-hidden="true"></i>
+                    </label>
+
+                </div>
+
+
+
+
 
 
 </body>
--- include/db local settings.php
+++ include/db local settings.php
@@ -1,8 +1,8 @@
 <?php
 
-$dsn = 'mysql:host=ctis256project.ctamhwtuall6.us-west-2.rds.amazonaws.com;dbname=test;charset=utf8mb4';
+$dsn = 'mysql:host=localhost;dbname=test;charset=utf8mb4';
 $user = 'root';
-$pass = 'Password123';
+$pass = '321';
 
 try {
   $db = new PDO ($dsn, $user, $pass);
