a20dfaf71c1f1b347f4d056acaec2a03354ee72e
Miraç Satıç <miracsa@yandex.com>
2018-05-18 23:13:54 +0300

    ajax part was written

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,42 @@
 
 <script type="text/javascript">
     $(function () {
-       alert("Hello!");
+
     });
+
+
+
+    function addBasket(id, amount) {
+
+        if(amount == null)
+            amount = 1;
+
+        var request = $.ajax({
+            url: 'add_to_basket.php?addToBasketAjax=true&product_id='+id+'&amount='+amount,
+            type: 'GET',
+            //data: { field1: "hello", field2 : "hello2"} ,
+            data: {},
+            contentType: 'application/json; charset=utf-8'
+        });
+
+        request.done(function (response) {
+            $.toast({
+                heading: 'Success',
+                text: response,
+                showHideTransition: 'slide',
+                icon: 'success',
+                position: {
+                    right: 20,
+                    top: 60
+                },
+            })
+        });
+
+        request.fail(function(jqXHR, textStatus) {
+            alert("AJAX Error! : " + textStatus);
+        });
+
+    }
 </script>
 
 </body>
