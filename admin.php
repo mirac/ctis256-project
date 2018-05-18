70d6374122ca9a531977f9812f43b6b4cb6e3e8f
Cagri <caggri@outlook.com>
2018-05-18 20:38:53 +0300

    admin panel without php

--- admin.php
+++ admin.php
@@ -11,16 +11,42 @@ if(empty($_SESSION) || $_SESSION['role'] == 0)
 
 else {
 
-    echo '<p class="h1">Welcome ' . $_SESSION['fullname'] . "</p>";
+    echo '<br><div id="addproduct"><p class="h1">Welcome ' . $_SESSION['fullname'] . "</p>";
 
 
 }
-/*
-if(empty($_SESSION['user']['role']))
-{
-    header("Location: index.php");
-    exit;
-}
+    ?>
 
-/*/
-?>
+
+
+<form method="POST" action="sendreg.php" class="form-signin">
+
+    <h1 class="h3 mb-3 font-weight-normal">Add Product</h1>
+
+    <label for="inputCategory" class="sr-only">Category of the product</label>
+    <input name="category" type="text" id="inputEmail" class="form-control" placeholder="Category" required autofocus>
+<br>
+    <label for="inputBrand" class="sr-only">Brand</label>
+    <input name="brand" type="text" id="inputEmail" class="form-control" placeholder="Brand" required>
+    <br>
+    <label for="Title" class="sr-only">Title</label>
+    <input name="Title" type="text" id="inputEmail" class="form-control" placeholder="Title" required>
+    <br>
+    <label for="Description" class="sr-only">Description</label>
+    <textarea name="Description" type="text" id="inputEmail" class="form-control" placeholder="Description" required>
+    </textarea>
+        <br>
+    <label for="Image" class="sr-only">Image</label>
+    <input name="Image" type="text" id="inputEmail" class="form-control" placeholder="Image" required>
+    <br>
+    <label for="Price" class="sr-only">Price</label>
+    <input name="Price" type="text" id="inputEmail" class="form-control" placeholder="Price" required>
+    <br>
+    <label>
+        <input type="checkbox" name="remember" value="remember-me" required> Promotional
+    </label>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Add Product</button>
+
+</form>
+</div>
+
